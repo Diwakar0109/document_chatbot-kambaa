e04ee88d48aa16fcb55745d2c0b1dd6f0b63e4c4
DIWAKAR R M <rmdiwakar29@gmail.com>
2025-08-11 13:05:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://document-chatbot-kambaa.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
